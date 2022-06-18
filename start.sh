4fc2cb9fe4ff2ccceed36ca7baf538234b9faead
nc4795gcp1 <107757305+nc4795gcp1@users.noreply.github.com>
2022-06-18 20:29:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#systemctl daemon-reload
-#sleep 5
-#systemctl enable --now manager-server
-#sleep 5
-#systemctl start manager-server
+service manager-server enable
+sleep 5
+service manager-server start
 while true; do sleep 1000; done
